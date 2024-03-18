c764a31ffea57587fc641a4923c82f5a0cc6b75e
Gakuraa <gabrieliwakura924@gmail.com>
2024-03-18 01:11:34 -0300

    botões radio e aplicando estilização teste

--- app/src/main/java/br/com/fiap/entradadousurio/MainActivity.kt
+++ app/src/main/java/br/com/fiap/entradadousurio/MainActivity.kt
@@ -3,30 +3,57 @@ package br.com.fiap.entradadousurio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.entradadousurio.ui.theme.EntradaDoUsuárioTheme
 
 class MainActivity : ComponentActivity() {
@@ -59,14 +86,30 @@ fun Greeting() {
         mutableStateOf("")
     }
 
+    var kotlin by remember {
+        mutableStateOf(false)
+    }
+    var java by remember {
+        mutableStateOf(false)
+    }
+    var cSharp by remember {
+        mutableStateOf(false)
+    }
+
+    var cor by remember {
+        mutableStateOf(Color.Green)
+    }
+
     Column(
         modifier = Modifier.padding(32.dp)
+            .background(cor)
     ){
         TextField(
             value = texto,
             onValueChange = {
                             letra -> texto = letra
             },
+            singleLine = true,
             modifier = Modifier.fillMaxWidth(),
             label = {
                 Text(text = "Qual o seu nome?")
@@ -93,7 +136,9 @@ fun Greeting() {
             onValueChange = {
                     letra -> idade = letra
             },
-            modifier = Modifier.fillMaxWidth()
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(top = 32.dp),
             label = {
                 Text(text = "Qual a sua idade?")
@@ -107,17 +152,28 @@ fun Greeting() {
                     contentDescription = "ícone home"
                 )
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            textStyle = TextStyle(
+                //color = Color.Red,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Color.Magenta,
+                unfocusedTextColor = Color.Gray,
+                cursorColor = Color.Cyan
+            )
         )
-        TextField(
+        OutlinedTextField(
             value = email,
-            onValueChange = {
-                    letra -> email = letra
+            onValueChange = { email = it
             },
-            modifier = Modifier.fillMaxWidth()
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(top = 32.dp),
             label = {
-                Text(text = "Qual seu email?")
+                Text(text = "Qual seu e-mail?")
             },
             placeholder = {
                 Text(text = "Digite seu endereço de e-mail")
@@ -128,10 +184,119 @@ fun Greeting() {
                     contentDescription = "ícone e-mail"
                 )
             },
+            shape = RoundedCornerShape(32.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Blue,
+                unfocusedBorderColor = Color.Black
+            ),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
         )
-    }
+        Spacer(modifier = Modifier.height(30.dp))
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Checkbox(checked = kotlin, onCheckedChange = {isSelected->
+                kotlin = isSelected
+            })
+            Text(text = "Kotlin")
+        }
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Checkbox(checked = java, onCheckedChange = {isSelected->
+                java = isSelected
 
+            },
+                colors = CheckboxDefaults.colors(checkedColor = Color.Green)
+                )
+            Text(text = "Java")
+        }
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Checkbox(checked = cSharp, onCheckedChange = {isSelected->
+                cSharp = isSelected})
+            Text(text = "C-Sharp")
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        var  linguagemSelecionada by remember {
+            mutableStateOf(0)
+        }
+
+        Row (verticalAlignment = Alignment.CenterVertically){
+            RadioButton(
+                selected = linguagemSelecionada == 1,
+                onClick = { linguagemSelecionada = 1 },
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = Color.Green,
+                    unselectedColor = Color.Magenta
+                )
+            )
+            Text(text = "Windows")
+        }
+        Row (verticalAlignment = Alignment.CenterVertically){
+            RadioButton(
+                selected = linguagemSelecionada == 2,
+                onClick = { linguagemSelecionada = 2 },
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = Color.Green,
+                    unselectedColor = Color.Magenta
+                )
+            )
+            Text(text = "MacOS")
+
+
+        }
+        Row (verticalAlignment = Alignment.CenterVertically){
+            RadioButton(
+                selected = linguagemSelecionada == 3,
+                onClick = { linguagemSelecionada = 3},
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = Color.Green,
+                    unselectedColor = Color.Magenta
+                )
+            )
+            Text(text = "Linux/GNU")
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        Row (horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Button(
+                onClick = { cor = Color.Red },
+                modifier = Modifier.size(width = 150.dp, height = 150.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF43C9AD),
+                    contentColor = Color(0xFF1B2D3F)
+                ),
+                border = BorderStroke(2.dp, Color(0xFF123456)),
+                shape = CircleShape
+            ) {
+               Row (
+                   horizontalArrangement = Arrangement.SpaceEvenly,
+                   modifier = Modifier.fillMaxWidth(),
+                   verticalAlignment = Alignment.CenterVertically
+               ){
+                   Icon(imageVector = Icons.Default.Share, contentDescription = "Share")
+                   Text(text = "Clique-me ")
+               }
+            }
+            Button(
+                onClick = { cor = Color.Yellow},
+                modifier = Modifier.size(width = 150.dp, height = 150.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF43C9AD),
+                    contentColor = Color(0xFF1B2D3F)
+                ),
+                border = BorderStroke(2.dp, Color(0xFF123456)),
+                shape = CircleShape
+            ) {
+                Row (
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Text(text = "Ou clique aqui")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
